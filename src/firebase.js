59c3717476857926f0df066fa24be367cff17712
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2022-01-01 15:44:55 +0530

    k

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
-import * as firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAjAef6fPsX9tkaSyhl2pyItCkUkpYA1LQ",
   authDomain: "twometersquareauth-development.firebaseapp.com",
-  databaseURL:"https://twometersquareauth-development-default-rtdb.firebaseio.com",
+  databaseURL:
+    "https://twometersquareauth-development-default-rtdb.firebaseio.com",
   projectId: "twometersquareauth-development",
   storageBucket: "twometersquareauth-development.appspot.com",
   messagingSenderId: "324002867272",
-  appId: "1:324002867272:web:c503beb32ab31f3596507d"
+  appId: "1:324002867272:web:c503beb32ab31f3596507d",
 };
 
 // Initialize Firebase
